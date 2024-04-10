e4406396c3d7bbb58b1f3157c7dfbb001a9e73a3
user <popovamariia2809@gmail.com>
2024-04-10 16:56:10 +0700

    Функции регистрации и аутентификации законченный вариант

--- app/Controller/Site.php
+++ app/Controller/Site.php
@@ -5,15 +5,19 @@ namespace Controller;
 use Model\Post;
 use Src\View;
 use Src\Request;
+use Model\User;
 
 
 class Site
 {
-    public function index(Request $request): string
+    public function signup(Request $request): string
     {
-        $posts = Post::where('id', $request->id)->get();
-        return (new View())->render('site.post', ['posts' => $posts]);
+        if ($request->method==='POST' && User::create($request->all())){
+            return new View('site.signup', ['message'=>'Вы успешно зарегистрированы']);
+        }
+        return new View('site.signup');
     }
+
 
 
     public function hello(): string
